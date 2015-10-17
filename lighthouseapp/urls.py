e0c2dd2bfe607ebae95cc0439d3839f8acc1d7aa
ubracklow <ubracklow@hotmail.com>
2015-10-17 14:49:37 +0200

    added app url file, template folder and start view

---
+++ lighthouseapp/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.start, name='start'),
+    #url(r'^host/(?P<pk>[0-9]+)/$', views.host, name = 'host'),
+    #url(r'^guests/(?P<pk>[0-9]+)/$', views.guests, name = 'guests'),
+    #url(r'^assignment_email/(?P<pk>[0-9]+)/$', views.assignment_email, name = 'assignment_email'),
+]
--- lighthouseapp/views.py
+++ lighthouseapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Resource
+#from .forms import ResourceForm
+
+def start(request):
+     return render(request, 'lighthouseapp/start.html')
 
 # Create your views here.
